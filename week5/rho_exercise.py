96c1eb15695a1440d97ecb7862659b41c5ab7774
Matilde Simões <up202108782@up.pt>
2024-10-29 09:28:18 +0000

    added extra5

--- week5/rho_exercise.py
+++ week5/rho_exercise.py
@@ -1,5 +1,6 @@
 from cryptography.hazmat.primitives import hashes
 import os
+import time
 
 L = 5 # output length in bytes
 
@@ -10,15 +11,18 @@ def H(X):
 	return (digest.finalize()[0:L])
 
 # Write a function that finds the collision and presents the values in which it occurred
-def rho(h0):
-	print("Hash is "+str(8*L)+" bits")
+def rho(h1):
+    print(f"Hash é de {8 * L} bits\n")
+    
+    
 
-	# Your code here!!
-	hi = "Nope"
+	print("\nEntradas que colidiram:")
+    print(f"Entrada 1: {entrada1.hex()}")
+    print(f"Entrada 2: {entrada2.hex()}")
+	print(f"Tempo: {end_time - initial_time:.6f} segundos")
+    print(f"Iterações: {iterations}")
 
-	return (h0, hi)
+    return entrada1, entrada2
 
 start = os.urandom(L)
-(h0, h1) = rho(start)
-print(h0)
-print(h1)
+h0, h1 = rho(start)
